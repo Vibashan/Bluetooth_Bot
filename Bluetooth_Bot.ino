8db2beae5572ed39192fa08230bc90e7f3c2a59f
Vibashan VS <34249645+Vibashan@users.noreply.github.com>
2018-02-01 02:07:00 +0530

    Add files via upload

---
+++ Bluetooth_Bot.ino
@@ -0,0 +1,65 @@
+#include<avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+void usart_int(void) {
+  UCSR0A=0x00;
+  UCSR0B=(1<< RXEN0)|(1<< TXEN0);//TRANSMIT AND RECEIVE ENABLE
+  UCSR0C=(1<<UCSZ01)|(1<<UCSZ00);//ASYNCHRONOUS, 8 BIT TRANSFER
+  UBRR0L= 103 ; //BAUD RATE 9600 
+}
+
+void usart_send(int ch )
+{
+  while(UCSR0A!=(UCSR0A|(1<<UDRE0)));//waiting for UDRE to become high
+  UDR0= ch;
+}
+
+int main(void)
+{
+  DDRB=0xFF;
+  DDRD=0b11110000;
+  float  Time;
+  int D;
+  usart_int();
+  unsigned int ch;
+  while(1)
+  {
+    
+    while(UCSR0A!= (UCSR0A|(1<<RXC0)));
+    
+    
+    ch=UDR0;
+    Serial.println(ch);
+    
+    if(ch==0)  //Stop
+    {
+      PORTD=0x00;
+    }
+    if(ch==1) //Backward
+    {
+      PORTD=0b10100000;
+      PORTB=0b00000110;
+    }
+    else if(ch==2) //Forward
+    {
+      PORTD=0b01010000;
+      PORTB=0b00000110;
+    }
+    else if(ch==3)  //Right
+    {
+      PORTD=0b10010000;
+      PORTB=0b00000110;
+    }
+    else if(ch==4) //Left
+    {
+      PORTD=0b01100000;
+      PORTB=0b00000110;
+    }
+
+    }
+      
+  }
+  
+  
+
