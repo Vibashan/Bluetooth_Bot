95a74b5a8cb0689a4bc28d6ca0b925b73bf6f2c5
Vibashan VS <34249645+Vibashan@users.noreply.github.com>
2018-02-01 02:09:30 +0530

    Update Bluetooth_Bot.ino

--- Bluetooth_Bot.ino
+++ Bluetooth_Bot.ino
@@ -2,36 +2,36 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
-void usart_int(void) {
+//USART Initialisation
+void usart_int(void) 
+{
   UCSR0A=0x00;
-  UCSR0B=(1<< RXEN0)|(1<< TXEN0);//TRANSMIT AND RECEIVE ENABLE
-  UCSR0C=(1<<UCSZ01)|(1<<UCSZ00);//ASYNCHRONOUS, 8 BIT TRANSFER
-  UBRR0L= 103 ; //BAUD RATE 9600 
+  UCSR0B=(1<< RXEN0)|(1<< TXEN0);// TRANSMIT AND RECEIVE ENABLE
+  UCSR0C=(1<<UCSZ01)|(1<<UCSZ00);// ASYNCHRONOUS, 8 BIT TRANSFER
+  UBRR0L= 103 ; // BAUD RATE 9600 
 }
 
 void usart_send(int ch )
 {
-  while(UCSR0A!=(UCSR0A|(1<<UDRE0)));//waiting for UDRE to become high
+  while(UCSR0A!=(UCSR0A|(1<<UDRE0)));// Waiting for UDRE to become high
   UDR0= ch;
 }
 
 int main(void)
 {
   DDRB=0xFF;
-  DDRD=0b11110000;
+  DDRD=0b11110000; // Setting all motor connections as Output
   float  Time;
   int D;
   usart_int();
   unsigned int ch;
   while(1)
   {
-    
+    //Waits for until it recivie data
     while(UCSR0A!= (UCSR0A|(1<<RXC0)));
     
-    
     ch=UDR0;
-    Serial.println(ch);
-    
+ 
     if(ch==0)  //Stop
     {
       PORTD=0x00;
